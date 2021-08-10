be71b977e0a09551f25069388f08b2c2a83b3d14
hrna-ox <henrique.aguiar@eng.ox.ac.uk>
2021-08-10 10:35:25 +0100

    Experiment run

--- data_process_utils.py
+++ data_process_utils.py
@@ -5,6 +5,20 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
 
+# Import samplers
+from imblearn.over_sampling import SMOTE, SMOTEN, SVMSMOTE, ADASYN
+
+# Import models
+from sklearn.svm import SVC as SVM
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.tree import DecisionTreeClassifier as Tree
+from sklearn.linear_model import LogisticRegression as LogReg
+from sklearn.neural_network import MLPClassifier as MLP
+
+# Import evaluation metrics
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score, confusion_matrix
+from sklearn.metrics import f1_score, recall_score, precision_score
+
 
 class type_detector():
     """
@@ -21,8 +35,33 @@ class data_processor():
     """
     Data Processor class.
     """
-    def __init__(self):
+    def __init__(self, mu = None, sigma = None):
+        self.mu    = mu
+        self.sigma = sigma
+
+    def fit(self, data):
+        mean_ = np.mean(data, axis = 0, keepdims = True)
+        std_  = np.std(data, axis = 0, keepdims = True)
+
+        self.mu = mean_
+        self.sigma = std_
+
         return None
+
+    def fit_transform(self, data):
+        self.fit(data)
+        return self.transform(data)
+
+
+    def transform(self, data):
+        return np.divide(data - self.mu, self.sigma)
+
+
+    def fit_train_test(self, data_train, data_test):
+        train_output = self.fit_transform(data_train)
+        test_output  = self.transform(data_test)
+
+        return train_output, test_output
 
 
 
@@ -38,7 +77,7 @@ class data_visualiser():
     def project(self, data, **kwargs):
         return TSNE(self.num_dimensions, **kwargs).fit_transform(data)
 
-    def plot(self, data, **kwargs):
+    def plot(self, data, ax, **kwargs):
         try:
             assert self._is_2D(data)
 
@@ -50,13 +89,11 @@ class data_visualiser():
         x, y = data[:, 0], data[:, 1]
 
         # Plot
-        fig, ax = plt.subplots()
         ax.scatter(x, y, **kwargs, label = "data")
 
         return ax
 
-
-    def plot_per_class(self, X,y, **kwargs):
+    def plot_per_class(self, X,y, ax,  **kwargs):
         """
         Scatter plot with different colors for each class
         """
@@ -69,29 +106,31 @@ class data_visualiser():
             X = self.project(X)
 
         # Compute num classes
-        num_classes_ = self._num_classes(self, y)
+        num_classes_ = self._num_classes(y)
 
-        for class_ in range(num_classes_):
+        # plot
+        for class_ in range(1, num_classes_ + 1):
 
             # Access data for the class
             X_class_ = X[self._get_class_ids(X, y, class_), :]
             x_scatter, y_scatter = X_class_[:, 0], X_class_[:, 1]
 
-            ax.scatter(x_scatter, y_scatter, c = self.colors[class_], label = "Class {}".format(class_))
+            ax.scatter(x_scatter, y_scatter, color = self.colors[class_], label = "Class {}".format(class_))
 
         return ax
 
-    def plot_pre_post_sampling(self, data_og, data_sampled, **kwargs):
+    def plot_pre_post_sampling(self, data_og, data_samp, ax, **kwargs):
         """
         Scatter plot comparing data before and after sampling
         """
         # Obtain corresponding data
-        X_og, y_og, X_samp, y_samp = data_og, data_sampled
+        X_og, y_og = data_og
+        X_samp, y_samp = data_samp
+
 
         # Make plots
-        fig, ax = plt.subplots(nrows = 1, ncols = 2)
-        ax[0] = self.plot_per_class(X_og, y_og)
-        ax[1] = self.plot_per_class(X_samp, y_samp)
+        ax[0] = self.plot_per_class(X_og, y_og, ax = ax[0])
+        ax[1] = self.plot_per_class(X_samp, y_samp, ax = ax[1])
 
         return ax
 
@@ -106,4 +145,97 @@ class data_visualiser():
 
     def _get_class_ids(self, X, y, class_id):
         return (y==class_id)
+
+
+
+
+
+class sampler():
+    def __init__(self, method, **kwargs):
+        if method == 'SMOTE':
+            self.sampler = SMOTE(**kwargs)
+
+        elif method == 'SMOTEN':
+            self.sampler = SMOTEN(**kwargs)
+
+        elif method == 'SVMSMOTE':
+            self.sampler = SVMSMOTE(**kwargs)
+
+        elif method == 'ADASYN':
+            self.sampler = ADASYN(**kwargs)
+
+        else:
+            print("Sampling method provided not valid!!")
+
+    def fit(self, X, y):
+        return self.sampler.fit(X, y)
+
+    def fit_resample(self, X, y):
+        return self.sampler.fit_resample(X, y)
+
+    def get_params(self, **kwargs):
+        return self.sampler.get_params(**kwargs)
+
+
+
+class model():
+    def __init__(self, model, **kwargs):
+        if model == "SVM":
+            self.model = SVM(**kwargs)
+
+        elif model == "KNN":
+            self.model = KNN(**kwargs)
+
+        elif model == "Tree":
+            self.model = Tree(**kwargs)
+
+        elif model == "Logistic":
+            self.model = LogReg(**kwargs)
+
+        elif model == "MLP":
+            self.model = MLP(**kwargs)
+
+        else:
+            print("Model name not accepted. Valid input names are \n"
+                  "SVM, KNN, Tree, Logistic, MLP")
+
+    def do_something_else(self, **kwargs):
+        print("Do something else! Feature to be added.")
+        return None
+
+
+    def fit_predict(self, X, y, X_test, **kwargs):
+        # Fit data
+        self.model.fit(X, y)
+
+        return self.model.predict(X_test)      # predict output
+
+
+class evaluator():
+    """
+    Evaluator class.
+    Makes prediction
+    """
+    def __init__(self, **kwargs):
+        self.bac = balanced_accuracy_score
+        self.roc = roc_auc_score
+        self.cm  = confusion_matrix
+        self.f1  = f1_score
+        self.rec = recall_score
+        self.pre = precision_score
+        self.results = []
+
+    def evaluate(self, y_true, y_pred):
+        output_dic = {
+            "balanced_accuracy": self.bac(y_true, y_pred),
+            "f1": self.f1(y_true, y_pred, average = "macro"),
+            "recall": self.rec(y_true, y_pred, average = "macro"),
+            "precision": self.pre(y_true, y_pred, average = "macro")
+            # "roc_auc_score": self.roc(y_true, y_pred, average = "macro", multi_class = "ovr"),
+            #"confusion matrix": self.cm(y_true, y_pred)
+        }
+
+        return output_dic
+
+
 
--- main.py
+++ main.py
@@ -4,21 +4,10 @@ import utils
 import pandas as pd
 import numpy as np
 import sklearn as skl
+from tqdm import tqdm
 
 import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
-
-# Import Imbalanced data section
-import imblearn as imbl
-from imblearn.datasets import fetch_datasets
-from imblearn.over_sampling import SMOTE, SMOTEN, SVMSMOTE, ADASYN
-
-# Load Models
-from sklearn.svm import SVC as SVM
-from sklearn.neighbors import KNeighborsClassifier as KNN
-from sklearn.tree import DecisionTreeClassifier as Tree
-from sklearn.linear_model import LogisticRegression as LogReg
-from sklearn.neural_network import MLPClassifier as MLP
+from sklearn.model_selection import train_test_split as TTS
 
 # Import from utils
 import data_process_utils as utils
@@ -93,27 +82,120 @@ import data_process_utils as utils
 #     results_comp = utils._compare_results(y_pred, y_over_pred, y_test)
 #     print(results_comp)
 
-# def main():
-
-# Load data
-glass_url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/glass.csv'
-data = pd.read_csv(glass_url, header=None)
-X, y = data.values[:, :-1], data.values[:, -1]
-
-# Process data
-processor = utils.data_visualiser()
-
-# Plot data
-fig, ax = plt.subplots()
-ax = processor.plot_per_class(X, y)
-
-plt.show()
 
 
 
+def main():
+        return None
 
-# if __name__ == "__main__":
-#     main()
+for i in range(1):
+
+        # binary_datasets = fetch_datasets()
+        glass_url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/glass.csv'
+        ecoli_url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/ecoli.csv'
+        thyroid_url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/new-thyroid.csv'
+
+        urls = [ecoli_url, thyroid_url, glass_url]
+        # urls = [glass_url]
+        overs_methods = ["SMOTE", "SMOTEN", "SVMSMOTE", "ADASYN"]    #,  custom_SMOTE]
+        strategies    = ["minority", "not minority", "not majority", "all", "auto"]
+        models = ["SVM", "KNN", "Tree", "Logistic"]
+
+        # save results
+        results_df = pd.DataFrame(data = np.nan, index = [], columns = ["dataset", "method", "strategy", "model", "og_or_samp",
+                                                            "balanced_accuracy", "f1", "precision", "recall"])
+
+        # Iterate through dataset, oversampling method and models.
+        for dataset_url in tqdm(urls):
+
+                # Load data
+                data = pd.read_csv(dataset_url, header=None)
+                X, y = data.values[:, :-1], data.values[:, -1]
+
+                # Process data
+                vis_processor = utils.data_visualiser()
+
+                # Plot data
+                # fig, ax = plt.subplots()
+                # ax = vis_processor.plot_per_class(ax = ax, X = X, y = y)
+
+                # plt.show()
+
+
+                # Process data
+                X_train_og, X_test_og, y_train_og, y_test_og = TTS(
+                        X, y, test_size = 0.3, random_state = 2323, shuffle = True, stratify = y)
+
+                # Sample data
+                for method_ in overs_methods:
+                        for oversampling_strategy_ in strategies:
+                                data_sampler = utils.sampler(method = method_,
+                                                        sampling_strategy = oversampling_strategy_)
+                                try:
+                                        X_train_samp, y_train_samp = data_sampler.fit_resample(X_train_og, y_train_og)
+                                except Exception as e:
+                                        print("Exception in resampling: ", e)
+                                        continue
+
+                                # Visualise data
+                                data_og, data_samp = (X_train_og, y_train_og), (X_train_samp, y_train_samp)
+                                # fig, axs = plt.subplots(nrows = 1, ncols = 2)
+                                # axs = vis_processor.plot_pre_post_sampling(ax = axs, data_og = data_og, data_samp = data_samp)
+                                # axs[0].set_title("Original Data projection")
+                                # axs[1].set_title("Sampled Data projection")
+                                # plt.show()
+
+                                # normalise for input
+                                processor_og, processor_samp = utils.data_processor(), utils.data_processor()
+
+                                x_norm_train_og, x_norm_test_og      = processor_og.fit_train_test(X_train_og, X_test_og)
+                                x_norm_train_samp, x_norm_test_samp  = processor_samp.fit_train_test(X_train_samp, X_test_og)
+
+                                # Run model
+                                for model_ in models:
+                                        model_og, model_samp = utils.model(model_), utils.model(model_)
+                                        y_pred_og  = model_og.fit_predict(x_norm_train_og, y_train_og, x_norm_test_og)
+                                        y_pred_samp= model_og.fit_predict(x_norm_train_samp, y_train_samp, x_norm_test_samp)
+
+                                        # Evaluate according to metrics
+                                        evaluator = utils.evaluator()
+
+                                        # Compute metrics for original
+                                        scores = list(evaluator.evaluate(y_test_og, y_pred_og).values())
+                                        new_row = [dataset_url, method_,oversampling_strategy_, model_, "og"] + scores
+                                        results_df = results_df.append(
+                                                pd.Series(dict(zip(results_df.columns, new_row))),
+                                                ignore_index = True
+                                        )
+
+                                        # Compute metrics for sampled
+                                        scores_samp = list(evaluator.evaluate(y_test_og, y_pred_samp).values())
+                                        new_row = [dataset_url, method_, oversampling_strategy_, model_, "samp"] + scores_samp
+                                        results_df = results_df.append(
+                                                pd.Series(dict(zip(results_df.columns, new_row))),
+                                                ignore_index = True
+                                        )
+
+
+        results_df.to_csv("Oversampling_results.csv")
+
+        # Print results difference
+        results_og = results_df.query('og_or_samp == "og"')
+        results_samp = results_df.query('og_or_samp == "samp"')
+        assert np.sum(results_og.iloc[:, :4].values != results_samp.iloc[:, :4].values)  == 0
+
+        # print average distribution
+        test = results_og
+        test.iloc[:, -4:] = test.iloc[:, -4:] - results_samp.iloc[:, -4:].values
+
+        # Print results by method
+        output_desc_ = test.groupby("method").apply(lambda x: x.iloc[:, -4:].describe())
+        print(output_desc_)
+
+
+
+if __name__ == "__main__":
+        main()
 
 
 
